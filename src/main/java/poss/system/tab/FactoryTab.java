c46c191a89c02d06e7953785612a7153cc581538
Ayato <chtk20063@g.nihon-u.ac.jp>
2023-01-15 23:12:41 +0900

    色々

--- src/main/java/poss/system/tab/FactoryTab.java
+++ src/main/java/poss/system/tab/FactoryTab.java
@@ -9,6 +9,6 @@ public class FactoryTab {
     public static final RegistoryObject<AbstractItemTab> BERGER_TAB = TABS.createRegistory("berger", () -> new NomalTab("H"));
     public static final RegistoryObject<AbstractItemTab> SIDE_TAB = TABS.createRegistory("side", () -> new NomalTab("S"));
     public static final RegistoryObject<AbstractItemTab> DRINK_TAB = TABS.createRegistory("drink", () -> new NomalTab("D"));
-    public static final RegistoryObject<AbstractItemTab> DESERT_TAB = TABS.createRegistory("sweets", () -> new NomalTab("S"));
+    public static final RegistoryObject<AbstractItemTab> DESERT_TAB = TABS.createRegistory("sweets", () -> new NomalTab("SW"));
 
 }
